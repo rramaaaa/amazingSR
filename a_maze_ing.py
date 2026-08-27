a7d9b3dbd5c283cb44e280c1c2e4a90a46e2355b
rama jaradat <rajarada@c2r7s10.irbid.42.school>
2026-08-27 20:21:01 +0300

    ..

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,9 +1,33 @@
 import sys
-from paths import read_config, Check_Corners, Ckech_not_in_lock
+import os
+from parsing import read_config, Check_Corners, Ckech_not_in_lock
 from maze import MazeGenerator
 from maze_printer import Maze_Printer
 from fortytwo_lock import FortyTwo_Lock, FortyTwo_Check
 from maze_analyzer import to_hexa, output
+
+
+def menu(input_num: int):
+    if ent == 1:
+        os.system("clear")
+        grid = obj.Create_Grid(rows, columns)
+        grid = FortyTwo_Lock(grid, rows, columns)
+        Ckech_not_in_lock(grid, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
+        grid = obj.Generate_Maze(grid, perfect)
+        grid = FortyTwo_Check(grid, rows, columns)
+        Maze_Printer(grid, rows, columns)
+        print()
+        print("=== A-Maze-ing ===")
+        print("1. Re-generate a new maze")
+        print("2. Show/Hide path from entry to exit")
+        print("3. Rotate maze colors")
+        print("4. Quit")
+    elif ent == 4:
+        exit()
+
+    else:
+        print("Please choice number (1-4)!")
+
 
 try:
     file_name = sys.argv[1]
@@ -14,18 +38,44 @@ try:
     exit_row, exit_column = config["EXIT"].split(",")
     perfect = config["PERFECT"]
     Check_Corners(rows, columns, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
-    obj = MazeGenerator()
-    grid = obj.Create_Grid(rows, columns)
-    grid = FortyTwo_Lock(grid, rows, columns)
-    Ckech_not_in_lock(grid, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
-    grid = obj.Generate_Maze(grid, perfect)
-    grid = FortyTwo_Check(grid, rows, columns)
-    #res = output(grid)
-    #to_hexa(res, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
-    #grid = FortyTwo(grid, rows, columns)
-    Maze_Printer(grid)
 
+    if rows < 12 or columns < 10:
+        print("Maze size is too small")
+        obj = MazeGenerator()
+        grid = obj.Create_Grid(rows, columns)
+        grid = obj.Generate_Maze(grid, perfect)
+        Maze_Printer(grid)
+
+    elif rows > 50 or columns > 50:
+        raise ValueError("Maze size is too large!\nPlease enter height and width values smaller than 50")
+
+    else:
+        obj = MazeGenerator()
+        grid = obj.Create_Grid(rows, columns)
+        grid = FortyTwo_Lock(grid, rows, columns)
+        Ckech_not_in_lock(grid, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
+        grid = obj.Generate_Maze(grid, perfect)
+        grid = FortyTwo_Check(grid, rows, columns)
+        Maze_Printer(grid, rows, columns)
+        print()
+        print("=== A-Maze-ing ===")
+        print("1. Re-generate a new maze")
+        print("2. Show/Hide path from entry to exit")
+        print("3. Rotate maze colors")
+
+        while True:
+            try:
+                ent = int(input("Choice? (1-4): "))
+                menu(ent)
+
+
+            except KeyboardInterrupt as e:
+                raise KeyboardInterrupt("\n")
+
+
+except KeyboardInterrupt :
+    print("\nQuitting the program")
 except Exception as e:
-    print(e)
+        print(e)
 
 #print(file_name)
--- maze_printer.py
+++ maze_printer.py
@@ -1,72 +1,26 @@
 from maze import MazeGenerator
 
-def Maze_Printer(grid: list[list["MazeGenerator.Cell"]], path: list[tuple[int, int]] = None) -> None:
-    if path is None:
-        path = []
-        
-    path_set = set(path)
-    rows = len(grid)
-    cols = len(grid[0])
+def Maze_Printer(grid: list[list["MazeGenerator.Cell"]], rows: int, columns: int, path: list[tuple[int, int]] = None) -> None:
 
-    def h_wall(i, j):
-        if i == 0 or i == rows:
-            return True
-        return grid[i - 1][j].Bottom
+    for i in range(columns):
+        print("████", end="")
+    print("██")
 
-    def v_wall(i, j):
-        if j == 0 or j == cols:
-            return True
-        return grid[i][j - 1].Right
-
-    CORNERS = {
-        (False, False, False, False): " ", (True,  False, False, False): "╵",
-        (False, True,  False, False): "╷", (False, False, True,  False): "╴",
-        (False, False, False, True):  "╶", (True,  True,  False, False): "│",
-        (False, False, True,  True):  "─", (True,  False, True,  False): "┘",
-        (True,  False, False, True):  "└", (False, True,  True,  False): "┐",
-        (False, True,  False, True):  "┌", (True,  True,  True,  False): "┤",
-        (True,  True,  False, True):  "├", (True,  False, True,  True):  "┴",
-        (False, True,  True,  True):  "┬", (True,  True,  True,  True):  "┼",
-    }
-
-    def corner_char(i, j):
-        up = v_wall(i - 1, j) if i > 0 else False
-        down = v_wall(i, j) if i < rows else False
-        left = h_wall(i, j - 1) if j > 0 else False
-        right = h_wall(i, j) if j < cols else False
-        return CORNERS[(up, down, left, right)]
-
-    lines = []
-    
-    # ANSI Colors
-    YELLOW_BG, YELLOW_FG = "\033[43m", "\033[33m"
-    GREEN_BG, GREEN_FG = "\033[42m", "\033[32m"
-    RESET = "\033[0m"
-
-    for i in range(rows + 1):
-        line = ""
-        for j in range(cols):
-            line += corner_char(i, j)
-            line += "───" if h_wall(i, j) else "   "
-        line += corner_char(i, cols)
-        lines.append(line)
-
-        if i < rows:
-            cell_line = ""
-            for j in range(cols):
-                cell_line += "│" if v_wall(i, j) else " "
-                
-
-                if grid[i][j].Lock:
-                    cell_line += f"{YELLOW_BG}{YELLOW_FG}███{RESET}"
-            
-                elif (i, j) in path_set:
-                    cell_line += f"{GREEN_BG}{GREEN_FG}███{RESET}"
-    
+    rows_num = rows * 2 + 1
+    for row in range(1, rows_num):
+        i = (row - 1) // 2
+        print("█", end="")
+        for column in range(columns):
+            if row % 2 != 0:
+                if grid[i][column].Lock:
+                    print("████", end="")
+                elif grid[i][column].Right:
+                    print("   █", end="")
                 else:
-                    cell_line += "   "
-
-            cell_line += "│" if v_wall(i, cols) else " "
-            lines.append(cell_line)
-
-    print("\n".join(lines))
+                    print("    ", end="")
+            else:
+                if grid[i][column].Bottom:
+                    print("████", end="")
+                else:
+                    print("   █", end="")
+        print("█")
