aef7200325c222e6f529e0439903d40924af54f4
rama jaradat <rajarada@c2r5s8.irbid.42.school>
2026-08-30 14:44:34 +0300

    fixed mypy

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -6,17 +6,26 @@ from maze import MazeGenerator
 from maze_printer import Maze_Printer, Maze_Printer_withPath
 from fortytwo_lock import FortyTwo_Lock, FortyTwo_Check
 from shortest_path import Finding_shortest_path
-from maze_analyzer import to_hexa, output
+# from maze_analyzer import to_hexa, output
 
 
-def menu(input_num: int, maze: list[list[MazeGenerator.Cell]], show_path: bool, entry: tuple[int, int], ext: tuple[int, int]) -> None:
+def menu(input_num: int,
+         maze: list[list[MazeGenerator.Cell]],
+         show_path: bool, entry: tuple[int, int],
+         ext: tuple[int, int]
+         ) -> None:
+
     if ent == 1:
         colors = ["\033[38;2;181;235;237m", "\033[38;2;245;230;168m"]
 
         os.system("clear")
         grid = obj.Create_Grid(rows, columns)
         grid = FortyTwo_Lock(grid, rows, columns)
-        Ckech_not_in_lock(grid, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
+        Ckech_not_in_lock(
+            grid,
+            int(entry_row), int(entry_column),
+            int(exit_row), int(exit_column)
+            )
         grid = obj.Generate_Maze(grid, perfect)
         grid = FortyTwo_Check(grid, rows, columns)
         Maze_Printer(grid, rows, columns, entry, ext, colors)
@@ -35,7 +44,11 @@ def menu(input_num: int, maze: list[list[MazeGenerator.Cell]], show_path: bool, 
 
         if show_path:
             path, _ = Finding_shortest_path(maze, entry, ext)
-            Maze_Printer_withPath(maze, rows, columns, entry, ext, colors, path)
+            Maze_Printer_withPath(
+                maze,
+                rows, columns,
+                entry, ext,
+                colors, path)
 
         else:
             Maze_Printer(maze, rows, columns, entry, ext, colors)
@@ -50,19 +63,22 @@ def menu(input_num: int, maze: list[list[MazeGenerator.Cell]], show_path: bool, 
     elif ent == 3:
         os.system("clear")
         all_colors = [
-            ["\033[38;2;243;182;210m", "\033[38;2;181;235;237m"], #Soft Pink and Light Cyan
-            ["\033[38;2;197;179;230m", "\033[38;2;181;235;237m"], #Soft Purple and Light Cyan
-            ["\033[38;2;154;255;155m", "\033[38;2;245;230;168m"], #Mint Green and Soft Yellow
-            ["\033[38;2;181;235;237m", "\033[38;2;243;182;210m"], #Light Cyan and Soft Pink
-            ["\033[38;2;85;191;194m", "\033[38;2;181;235;237m"],  #Teal and Light Cyan
-            ["\033[38;2;141;216;232m", "\033[38;2;245;230;168m"], #Soft Blue and Soft Yellow
-            ["\033[38;2;243;182;210m", "\033[38;2;245;230;168m"]  #Soft Pink and Soft Yellow
+            ["\033[38;2;243;182;210m", "\033[38;2;181;235;237m"],
+            ["\033[38;2;197;179;230m", "\033[38;2;181;235;237m"],
+            ["\033[38;2;154;255;155m", "\033[38;2;245;230;168m"],
+            ["\033[38;2;181;235;237m", "\033[38;2;243;182;210m"],
+            ["\033[38;2;85;191;194m", "\033[38;2;181;235;237m"],
+            ["\033[38;2;141;216;232m", "\033[38;2;245;230;168m"],
+            ["\033[38;2;243;182;210m", "\033[38;2;245;230;168m"]
         ]
 
         colors = random.choice(all_colors)
         if show_path:
             path, _ = Finding_shortest_path(maze, entry, ext)
-            Maze_Printer_withPath(maze, rows, columns, entry, ext, colors, path)
+            Maze_Printer_withPath(maze,
+                                  rows, columns,
+                                  entry, ext,
+                                  colors, path)
 
         else:
             Maze_Printer(maze, rows, columns, entry, ext, colors)
@@ -91,7 +107,10 @@ try:
     perfect = config["PERFECT"]
     entry = (int(entry_row), int(entry_column))
     ext = (int(exit_row), int(exit_column))
-    Check_Corners(rows, columns, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
+    Check_Corners(rows, columns,
+                  int(entry_row), int(entry_column),
+                  int(exit_row), int(exit_column)
+                  )
 
     if rows < 12 or columns < 10:
         print("Maze size is too small")
@@ -101,7 +120,10 @@ try:
         Maze_Printer(grid)
 
     elif rows > 50 or columns > 50:
-        raise ValueError("Maze size is too large!\nPlease enter height and width values smaller than 50")
+        raise ValueError(
+            "Maze size is too large!\n"
+            "Please enter height and width values smaller than 50"
+            )
 
     else:
         colors = ["\033[38;2;181;235;237m", "\033[38;2;245;230;168m"]
@@ -109,10 +131,15 @@ try:
         obj = MazeGenerator()
         grid = obj.Create_Grid(rows, columns)
         grid = FortyTwo_Lock(grid, rows, columns)
-        Ckech_not_in_lock(grid, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
+        Ckech_not_in_lock(grid,
+                          int(entry_row), int(entry_column),
+                          int(exit_row), int(exit_column)
+                          )
         grid = obj.Generate_Maze(grid, perfect)
         grid = FortyTwo_Check(grid, rows, columns)
-        Maze_Printer(grid, rows, columns,entry, ext, colors) #Light Cyan and Soft Yellow
+        Maze_Printer(grid,
+                     rows, columns,
+                     entry, ext, colors)  # Light Cyan and Soft Yellow
         print()
         print("=== A-Maze-ing ===")
         print("1. Re-generate a new maze")
@@ -134,17 +161,18 @@ try:
                 else:
                     menu(ent, grid, path, entry, ext)
 
-
-            except KeyboardInterrupt as e:
+            except KeyboardInterrupt:
                 raise KeyboardInterrupt("")
 
 
-except KeyboardInterrupt :
+except KeyboardInterrupt:
     print("\nQuitting the program")
 except Exception as e:
-        print(e)
+    print(e)
 
 
-#res = output(grid)
-#to_hexa(grid, res, int(entry_row), int(entry_column), int(exit_row), int(exit_column))
-#print(file_name)
+# res = output(grid)
+# to_hexa(grid, res,
+#        int(entry_row), int(entry_column),
+#        int(exit_row), int(exit_column))
+# print(file_name)
